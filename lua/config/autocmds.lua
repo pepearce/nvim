279e9622b2f59b3c25433e1665921a4c85a8e019
ppearce <paul.vpearce@gmail.com>
2025-08-01 14:51:18 +0200

    upadte nvim plugins and remove harpoon

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({ async = false, id = args.data.client_id })
+      end,
+    })
+  end,
+})
